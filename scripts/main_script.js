d3ef078f22a146445d01e4a78b3112cc5a524ad0
ChaoticBrave <alisdairpage1@gmail.com>
2024-04-30 00:15:40 +0200

    Updating portfolio page

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger');
 const no_burger = document.querySelector('.menu');
-const width = document.documentElement.clientWidth;
+const tab_width = window.matchMedia("(max-width: 50rem)");
+const phone_width = window.matchMedia("(max-width: 30rem)");
+const page = document.querySelector('.page');
 
 burger.addEventListener('click', function () {
 	const burIc = this.querySelector('.burger-icon');
@@ -9,11 +11,19 @@ burger.addEventListener('click', function () {
 		burIc.style.display = "inline-block";
 		xIc.style.display = "none";
 		no_burger.style.display = "none";
+		if (tab_width.matches) {
+			page.style.paddingTop = "8rem";
+		}
+		if (phone_width.matches) {
+			page.style.paddingTop = "9rem";
+		}
+		
 	}
 	else {
 		xIc.style.display = "inline-block";
 		no_burger.style.display = "block";
 		burIc.style.display = "none";
+		page.style.paddingTop = "15rem";
 	}
 }
 );
